1e7374263025ed3c5461453f11b586d9f462736b
grzesiakmarcin <mk.grzesiak@gmail.com>
2020-04-28 21:41:49 +0200

    codingBat.com String -1

--- src/strings/Strings.java
+++ src/strings/Strings.java
@@ -7,21 +7,63 @@ public class Strings {
         String str = "Java";
         String out = "[[]]";
 
-        Stringus s1 = new Stringus();
-        s1.makeOutWord(out,str);
+        StringOne s1 = new StringOne();
+
+        s1.firstTwo(str);
 
     }
 
 }
 
 
-class Stringus{
-
-
+class StringOne{
 
     public String makeOutWord(String out, String str) {
             System.out.println(out.substring(0,2) +str+out.substring(2,4));
-            return  out;
+
+    return  out;
+    }
+        public String firstTwo(String str) {
+
+            if (str.length()<3){
+                return str;
+            }
+
+            else
+            {
+                return str.substring(0,2);
+            }
+
+        }
+
+    public String firstHalf(String str) {
+        int lenght= str.length()/2;
+
+        return str.substring(0,lenght);
     }
 
+    public String withoutEnd(String str) {
+        return str.substring(0+1,str.length()-1);
+    }
+
+    public String comboString(String a, String b) {
+        if(a.length()<b.length()){
+            return a+b+a;
+        }
+        else{
+            return b+a+b;
+        }
+    }
+    public String left2(String str) {
+
+        if(str.length()>2){
+            return str.substring(2,str.length())+str.substring(0,2);
+        }
+        return str;
+
+    }
+
+
 }
+
+
