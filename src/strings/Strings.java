06b0633ce460eff07290e7c2571bc4137c60dfbf
Marcin <mk.grzesiak@gmail.com>
2020-05-05 22:20:57 +0200

    coding bat cd

--- src/strings/Strings.java
+++ src/strings/Strings.java
@@ -175,6 +175,38 @@ class StringOne{
 
     }
 
+    public String lastChars(String a, String b) {
+
+        if(a.length()>0 && b.length()>0){
+            return a.substring(0,1)+b.substring(b.length()-1);
+        }
+
+        else if (a.length()==0 && b.length()>0){
+            return "@" +b.substring(b.length()-1);
+        }
+
+        else if (a.length()>0 && b.length()==0){
+            return a.substring(0,1)+"@";
+        }
+
+
+        return "@@" ;
+
+    }
+    public String conCat(String a, String b) {
+        if(a.length()==0||b.length()==0){
+            return a+b;
+        }
+        else if ((a.charAt(a.length()-1)==b.charAt(0))){
+            return  a.substring(0,a.length()-1)+b;
+        }
+
+        return a+b;
+    }
+
+
+
+
 
 
 }
