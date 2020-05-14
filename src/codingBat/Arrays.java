77c4789945fce6cdcec903d7390cf2f1408212af
Marcin <mk.grzesiak@gmail.com>
2020-05-14 21:46:15 +0200

    arrays exercises koniec na dzis

--- src/codingBat/Arrays.java
+++ src/codingBat/Arrays.java
@@ -9,6 +9,11 @@ Basic array problems -- no loops.. Use a[0], a[1], ... to access elements in an 
  s.length() is for Strings). Allocate a new array like this: int[] a = new int[10]; // length 10 array
  */
 
+import com.sun.tools.javac.util.ArrayUtils;
+import sun.security.util.ArrayUtil;
+
+import java.util.stream.IntStream;
+
 public class Arrays {
     public static void main(String[] args) {
 
@@ -243,6 +248,14 @@ public boolean sameFirstLast(int[] nums){
         }
 
         return b;
+    }
+
+    public int[] plusTwo(int[] a, int[] b) {
+        int[] newArray = {a[0],a[1],b[0],b[1]};
+        return newArray;
+
+
+
     }
 
 
