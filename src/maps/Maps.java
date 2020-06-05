728ea9e8844e3dc869f58e84533818917955bda1
Marcin <mk.grzesiak@gmail.com>
2020-06-05 22:30:04 +0200

    mapy

--- src/maps/Maps.java
+++ src/maps/Maps.java
@@ -8,25 +8,70 @@ public class Maps {
 
         HashMap<Integer, String> myMap = new HashMap<Integer, String>();
 
-        myMap.put(1,"Bacik");
-        myMap.put(2,"Bielnik");
+        myMap.put(1, "Bacik");
+        myMap.put(2, "Bielnik");
         myMap.put(3, "Duczmal");
         myMap.put(4, "Dylejko");
-        myMap.put(5,"Grzesiak");
+        myMap.put(5, "Grzesiak");
 
-        myMap.put(1,"Anakonda");
+        myMap.put(1, "Anakonda");
 
-        for (String stringus: myMap.values()){
+        for (String stringus : myMap.values()) {
             System.out.println(stringus);
 
         }
 
 
-        for (Map.Entry<Integer, String> enty : myMap.entrySet()){         // iterowanie po kluczu i wartosci
+        for (Map.Entry<Integer, String> enty : myMap.entrySet()) {         // iterowanie po kluczu i wartosci
             System.out.println(enty);
         }
 
 
+        Map<Integer, String> mapa = new HashMap();
 
+        Pracownik[] pracownicy = {
+                new Pracownik("Agnieszka"),
+                new Pracownik("Janek"),
+                new Pracownik("Marta"),
+                new Pracownik("Jacek"),
+                new Pracownik("Henryk"),
+                new Pracownik("Tyberiusz"),
+        };
+
+        for (Pracownik ludzik : pracownicy){
+            mapa.put(ludzik.getID(), ludzik.getImie());
+        }
+
+
+        System.out.println("\n"+ "Oto mapa pracowników: "+ "\n " +mapa);
+
+    }
+}
+
+class Pracownik{
+
+    public Pracownik(){
+        i++;
+        ID=i;
+    }
+
+    public Pracownik(String imie) {
+        this();
+        this.imie = imie;
+
+
+    }
+
+    private String imie;
+    private int ID;
+    public static int i=0;
+
+
+    public String getImie() {
+        return imie;
+    }
+
+    public int getID() {
+        return ID;
     }
 }
