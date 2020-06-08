3ee3808a168b4866616a0936b033733c9337b765
Marcin <mk.grzesiak@gmail.com>
2020-06-08 23:24:51 +0200

    coding bat maps cd

--- src/codingBat/Maps.java
+++ src/codingBat/Maps.java
@@ -120,6 +120,35 @@ public class Maps {
     }
 
 
+    public Map<String, String> mapAB2(Map<String, String> map) {
+
+        if(map.containsKey("a")&&map.containsKey("b")){
+            if(map.get("a").equals(map.get("b")) ){
+                map.remove("a");
+                map.remove("b");
+            }
+
+        }
+
+
+        return map;
+    }
+
+    public Map<String, String> mapAB3(Map<String, String> map) {
+
+
+        if(map.containsKey("a")&& !map.containsKey("b")){
+            map.put("b",map.get("a"));
+        }
+        if(map.containsKey("b")&& !map.containsKey("a")){
+            map.put("a",map.get("b"));
+        }
+
+
+        return map;
+
+    }
+
 
 
 }
