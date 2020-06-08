59c2592086b370f1fbe0e1f48f853e322e6eaf22
Marcin <mk.grzesiak@gmail.com>
2020-06-08 22:57:58 +0200

    coding bat maps c d

--- src/codingBat/Maps.java
+++ src/codingBat/Maps.java
@@ -85,4 +85,14 @@ public class Maps {
         return map;
     }
 
+    public Map<String, String> topping1(Map<String, String> map) {
+        if(map.containsKey("ice cream")){
+            map.put("ice cream","cherry");
+
+        }
+        map.put("bread","butter");
+        return map;
+    }
+
+
 }
