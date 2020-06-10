b61bd89e8c89932e865eee28fa99d5f6d3234a91
Marcin <mk.grzesiak@gmail.com>
2020-06-10 20:10:43 +0200

    maps cd

--- src/codingBat/Maps2.java
+++ src/codingBat/Maps2.java
@@ -3,6 +3,10 @@ package codingBat;
 import java.util.HashMap;
 import java.util.Map;
 
+
+/*
+https://codingbat.com/java/Map-2
+ */
 public class Maps2 {
     public static void main(String[] args) {
 
@@ -23,5 +27,44 @@ public class Maps2 {
 
          return map;
      }
+
+     public Map<String, Integer> wordLen(String[] strings) {
+         HashMap <String, Integer> myMap = new HashMap<String, Integer>();
+
+         for (String stringus: strings){
+             myMap.put(stringus, stringus.length());
+         }
+
+         return myMap;
+     }
+
+     public Map<String, String> pairs(String[] strings) {
+         HashMap<String, String> myMap = new HashMap <String, String>();
+
+         for (String stringus: strings){
+             myMap.put(stringus.substring(0,1),stringus.substring(stringus.length()-1,stringus.length()));
+
+         }
+
+         return myMap;
+     }
+
+     /*
+     Map-2 > wordCount  -> wykminic z Mateuszem
+
+     public Map<String, Integer> wordCount(String[] strings) {
+  HashMap <String, Integer>myMap = new HashMap <String, Integer>();
+
+  for (int i=0; i<wirdCount.length; i++)
+  {
+    if()
+  }
+
+
+  return myMap;
+}
+
+      */
+
 
  }
