a0ddef3e61f2be585266a76250b5b5a7215f2b4d
Marcin <mk.grzesiak@gmail.com>
2020-06-11 20:05:30 +0200

    cos nie dziala

--- src/codingBat/Maps2.java
+++ src/codingBat/Maps2.java
@@ -1,7 +1,9 @@
 package codingBat;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 
 /*
@@ -10,61 +12,88 @@ https://codingbat.com/java/Map-2
 public class Maps2 {
     public static void main(String[] args) {
 
+        Kodzik d = new Kodzik();
 
+
+        String[] tablica = new String[] {"poniedziałek", "wtorek", "sroda", "czwartek", "piatek", "sobota", "jutro poniedzialek"};
+
+
+        d.wordCount(tablica);
+
+
+
+        // tablica wejściowa
+        byte[] tablicaBajtów = new byte[] { 5, 5, 6, 6, 6, 7 };
+        // mapa pomocnicza
+        Map<Integer, Integer> mapa = new HashMap<Integer, Integer>();
+        // zliczanie wystąpień dla każdej wartośći
+        for (byte bajt : tablicaBajtów) {
+            if (mapa.containsKey(Integer.valueOf(bajt))) {
+                Integer wartość = mapa.get(Integer.valueOf(bajt));
+                mapa.put(Integer.valueOf(bajt), wartość + 1);
+            } else {
+                mapa.put(Integer.valueOf(bajt), 1);
+            }
+        }
 
     }
 
 }
- class Kodzik{
 
-     public Map<String, Integer> word0(String[] strings) {
+class Kodzik {
 
-         HashMap<String, Integer> map = new HashMap<String, Integer>();
+    public Map<String, Integer> word0(String[] strings) {
 
-         for (String  pozcyjaTablicy : strings){
-             map.put(pozcyjaTablicy,0);
-         }
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
 
-         return map;
-     }
+        for (String pozcyjaTablicy : strings) {
+            map.put(pozcyjaTablicy, 0);
+        }
 
-     public Map<String, Integer> wordLen(String[] strings) {
-         HashMap <String, Integer> myMap = new HashMap<String, Integer>();
+        return map;
+    }
 
-         for (String stringus: strings){
-             myMap.put(stringus, stringus.length());
-         }
+    public Map<String, Integer> wordLen(String[] strings) {
+        HashMap<String, Integer> myMap = new HashMap<String, Integer>();
 
-         return myMap;
-     }
+        for (String stringus : strings) {
+            myMap.put(stringus, stringus.length());
+        }
 
-     public Map<String, String> pairs(String[] strings) {
-         HashMap<String, String> myMap = new HashMap <String, String>();
+        return myMap;
+    }
 
-         for (String stringus: strings){
-             myMap.put(stringus.substring(0,1),stringus.substring(stringus.length()-1,stringus.length()));
+    public Map<String, String> pairs(String[] strings) {
+        HashMap<String, String> myMap = new HashMap<String, String>();
 
-         }
+        for (String stringus : strings) {
+            myMap.put(stringus.substring(0, 1), stringus.substring(stringus.length() - 1));
 
-         return myMap;
-     }
+        }
+
+        return myMap;
+    }
 
      /*
      Map-2 > wordCount  -> wykminic z Mateuszem
-
-     public Map<String, Integer> wordCount(String[] strings) {
-  HashMap <String, Integer>myMap = new HashMap <String, Integer>();
-
-  for (int i=0; i<wirdCount.length; i++)
-  {
-    if()
-  }
+*/
 
 
-  return myMap;
+    public Map<String, Integer> wordCount(String[] strings) {
+
+        Map<String, Integer> myMap = new HashMap<String, Integer>();
+
+        for(String stringus: strings) {
+            if (myMap.containsKey(stringus)){
+                myMap.put(stringus,wartosc+1);
+            }
+
+            else {
+                myMap.put(stringus,1);
+            }
+        }
+        return myMap;
+    }
+
+
 }
-
-      */
-
-
- }
