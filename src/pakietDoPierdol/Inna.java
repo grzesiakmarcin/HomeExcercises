c301dca3ea61ea775b9d29f760d88431a7b78612
Marcin <mk.grzesiak@gmail.com>
2020-07-02 17:35:31 +0200

    jakies sprawdzenie

---
+++ src/pakietDoPierdol/Inna.java
@@ -0,0 +1,23 @@
+package pakietDoPierdol;
+
+import java.util.Scanner;
+import java.util.function.DoubleToIntFunction;
+
+public class Inna {
+    public static void main(String[] args) {
+//
+//czemu drukuje az 10 cyfr?
+
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println(i);
+//            for (int j = 0; j < 5; j++) {
+//                System.out.println(j);
+//                break;
+//            }
+//        }
+//
+//    }
+//
+
+    }
+}
--- src/pakietDoPierdol/main.java
+++ src/pakietDoPierdol/main.java
@@ -4,19 +4,62 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        String wyraz = " string";
-        String wyraz2 = "     s  t r i n  g";
+        int waterStandard = 200;
+        int milkStandard = 50;
+        int coffeeStandard = 15;
 
-        String trim1 = wyraz.trim();
-        String trim2 = wyraz2.trim().replace(" ","");
-
-
-        System.out.println(trim1.equals(trim2));
-        System.out.println(trim1);
-        System.out.println(trim2);
+        int water = 60_00;
+        int milk = 15_000;
+        int coffee = 4_500;
 
 
+
+
+        System.out.println("Write how many ml of water the coffee machine has: ");
+        System.out.println(water);
+        System.out.println("Write how many ml of milk the coffee machine has: ");
+        System.out.println(milk);
+        System.out.println("Write how many grams of coffee beans the coffee machine has: ");
+        System.out.println(coffee);
+        System.out.println("Write how many cups of coffee you will need: ");
+        int needed = sc.nextInt();
+
+        int waterNeeded = needed*waterStandard;
+        int milkNeeded = needed*milkStandard;
+        int coffeeNeeded = needed*coffeeStandard;
+        int countNeeded = needed;
+        int howManyWeCan=0;
+
+
+        boolean exaclyOneEnough =
+                waterNeeded == water &&
+                milkNeeded == milk &&
+                coffeeNeeded  == coffee;
+
+        boolean moreEnough = waterNeeded<= water&&
+                              milkNeeded <= milk &&
+                              coffeeNeeded <= coffee;
+
+        for(int i =needed; i>0; i--){
+            if((water-(waterStandard*i)>=0)&&(milk-(milkStandard*i)>=0) &&(coffee-(coffeeStandard*i)>=0)){
+                howManyWeCan=i;
+                break;
+            }
+        }
+
+
+
+
+
+        if(exaclyOneEnough){System.out.println("Yes, I can make that amount of coffee");}
+        else if (moreEnough){
+            System.out.println("Yes, I can make that amount of coffee (and even "+(howManyWeCan-1)+" more than that");
+        }
+        else{
+            System.out.println("No, I can make only "+howManyWeCan +" cup(s) of coffee");
+        }
     }
 
 
