f26aec13b85afff5fd4c0599649a96f2dfc0d29c
grzesiakmarcin <mk.grzesiak@gmail.com>
2020-07-11 19:37:29 +0200

    terbery operator cd

--- src/terneryOperator/ternery.java
+++ src/terneryOperator/ternery.java
@@ -3,6 +3,11 @@ package terneryOperator;
 public class ternery {
     public static void main(String[] args) {
 
+        /*
+        ?   zakancza warunek
+        :   oddziela odpowiedzi
+         */
+
 
         String napis = "Dużymi";
 
@@ -10,7 +15,44 @@ public class ternery {
         String firsName = napis.equals("Dużymi") ? "Marcin" : "marcin";
 
         System.out.println(firsName);
-        System.out.println(wiek);
+
+
+        int a1=300;
+        int b12 = 3332;
+        int max= a1>b12  ?  a1 : b12;
+
+        System.out.println(max);
+/*
+Ternary Operator as Null Check
+
+You can use the Java ternary operator as a shorthand for null checks before calling a method on an object. Here is an example:
+
+String value = object != null ? object.getValue() : null;
+
+This is equivalent to, but shorter than this code:
+
+String value = null;
+if(object != null) {
+    value = object.getValue();
+}
+
+As you can see, both of these code examples avoid calling object.getValue() if the object reference is null, but the first code example is a bit shorter and more elegant.
+ */
+        String value = firsName != null ? firsName : null;
+        System.out.println(value);
+
+
+
+        boolean b1= false;
+        boolean b2= true;
+        boolean b3= false;
+
+
+
+        boolean result = (b1 && b2) ? b1 : (b2 || b3) ? b2 : b3;
+
+        System.out.println(result);
+
 
     }
 }
