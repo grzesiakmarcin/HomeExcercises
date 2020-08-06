7a8b05f0c7126f1d435c4cf6db70b72d835dce2f
Marcin <mk.grzesiak@gmail.com>
2020-08-06 23:03:17 +0200

    enums

--- src/enums/InnyPrzyklad.java
+++ src/enums/InnyPrzyklad.java
@@ -1,4 +1,21 @@
 package enums;
 
+enum TshirtSize{
+
+    S,M,L,XL,XXL;
+
+
+
+}
+
 public class InnyPrzyklad {
+    public static void main(String[] args) {
+        movieTypes mT=movieTypes.BORING;
+        System.out.println(mT);
+        System.out.println("---");
+        System.out.println(mT.ordinal());
+
+
+
+    }
 }
