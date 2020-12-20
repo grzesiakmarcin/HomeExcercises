62ad80ba57303e1fb21c50b44cd2e36e39a49f30
grzesiakmarcin <mk.grzesiak@gmail.com>
2020-12-20 21:32:09 +0100

    koniec na dziisaj

---
+++ src/opp/Person.java
@@ -0,0 +1,63 @@
+package opp;
+
+import java.util.Date;
+
+public class Person {  
+    protected String name;
+    protected int yearOfBirth;
+    protected String address;
+
+    public Person(String name, int yearOfBirth, String address) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", yearOfBirth=" + yearOfBirth +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+
+    // getters and setters
+}
+
+class Employee extends Person {
+
+    protected Date startDate;
+    protected Long salary;
+
+    public Employee(String name, int yearOfBirth, String address,  Long salary) {
+        super(name, yearOfBirth, address); // invoking a constructor of the superclass
+
+        this.startDate = startDate;
+        this.salary = salary;
+    }
+
+    // getters and setters
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", yearOfBirth=" + yearOfBirth +
+                ", address='" + address + '\'' +
+                ", startDate=" + startDate +
+                ", salary=" + salary +
+                '}';
+    }
+}
+
+class JakisMain{
+    public static void main(String[] args) {
+        Employee pracownik1 = new Employee("Stas",2020/1/02, "Leszno",5006L);
+
+        System.out.println(pracownik1.toString());
+
+
+    }
+}
