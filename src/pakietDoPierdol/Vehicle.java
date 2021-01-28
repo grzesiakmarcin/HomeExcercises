15877b690f54ecc049fb4f795d9b27d2ff5eaa07
grzesiakmarcin <mk.grzesiak@gmail.com>
2021-01-28 22:19:08 +0100

    ciśniemy

--- src/pakietDoPierdol/Vehicle.java
+++ src/pakietDoPierdol/Vehicle.java
@@ -12,6 +12,11 @@ public class Vehicle {
         public void setRegNumber(String regNumber) {
             this.regNumber = regNumber;
         }
+
+
+        public void hello (){
+            System.out.println("nadklasa vehicle");
+        }
     }
 
     class Car extends Vehicle {
@@ -25,12 +30,27 @@ public class Vehicle {
         public void setNumberOfSeats(int numberOfSeats) {
             this.numberOfSeats = numberOfSeats;
         }
+       @Override
+        public void hello(){
+           System.out.println("metoda hello w podklasie");
+
+       }
     }
 
     class Start{
         public static void main(String[] args) {
             Vehicle car = new Car();
 
+
+
+           // Car jakas = new Vehicle(); - ni chuja nie dziala i nie zadziala
+
+
+            Vehicle jakis = new Vehicle();
+
+            Car jakisInny = new Car();
+
+         car.hello();
 
             car.setRegNumber("ABC-123"); // (2)
            // car.setNumberOfSeats(5); // (3)
---
+++ src/strings/polimorphism/Notification.java
@@ -0,0 +1,73 @@
+package strings.polimorphism;
+
+public class Notification {
+
+    protected String msg;
+
+    public Notification(String msg) {
+        this.msg = msg;
+    }
+
+    public void show() {
+        System.out.println(getMsg());
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
+
+class Warning extends Notification {
+
+    public Warning(String msg) {
+        super(msg);
+    }
+
+    @Override
+    public String getMsg() {
+        return "WARN: " + msg;
+    }
+}
+
+class Alarm extends Notification {
+
+    public Alarm(String msg) {
+        super(msg);
+    }
+
+    @Override
+    public void show() {
+        System.out.println("ALARM: " + msg);
+    }
+}
+
+
+class Main {
+    public static void main(String[] args) {
+
+
+        Notification notif = new Notification("No problems");
+        Notification warn = new Warning("Money ends");
+        Notification alarm = new Alarm("The ship sank");
+
+ //       warn.show();             //outputs "No problems" - niet
+//        System.out.println(
+        System.out.println(  alarm.getMsg()) ;//returns "ALARM: The ship sank"  da
+//        System.out.println( warn.getMsg());         //returns "WARN: Money ends"
+//       notif.show();         // outputs "No problems"  da
+//       alarm.show();        //outputs "ALARM: The ship sank"da
+
+
+
+        Person osoba = new Person("Ola",166);
+
+        Patient pacjent = new Patient("Ewa",39, 123);
+
+
+        System.out.println(osoba.toString());
+
+        System.out.println();
+        pacjent.toString();
+
+    }
+}
