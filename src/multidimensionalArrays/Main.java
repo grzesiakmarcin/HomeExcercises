70b9859bfce05cd4ea4c60b99f36cb6ddaa80374
grzesiakmarcin <mk.grzesiak@gmail.com>
2021-03-09 10:38:21 +0100

    zly wynik

--- src/multidimensionalArrays/Main.java
+++ src/multidimensionalArrays/Main.java
@@ -7,144 +7,90 @@ public class Main {
 
 
         int[][] cinema = {
-                {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1},
-                {1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0},
-                {0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0},
-                {1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0},
-                {1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1},
-                {0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0},
-                {0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0},
-                {1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0},
-                {1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0},
-                {0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0},
-                {0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0},
-                {1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1},
-                {0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0},
-                {1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0},
-                {0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1},
-                {1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0},
-                {1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1},
-                {0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0}};
 
+                {1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0},
+                {1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1},
+                {1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1},
+                {0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1},
+                {0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1},
+                {0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1},
+                {0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0},
+                {1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1},
+                {1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0},
+                {1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0},
+                {1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1},
+                {1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0},
+                {1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1},
+                {1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1},
+                {0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1},
+                {0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0},
+                {1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0},
+                {0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1},
+                {1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0}};
 
-        int found0InARow = 0;
-        int currentRowTEST = 0;
-        boolean booleanForTest = false;
-        int anotherZero = 0;
+        int howManyTickersNeeded = 5;
+        int avaliableSeatsNextToEachOther = 0;
+        int currentRow = 0;
+        boolean stopSearching = false;
+        boolean anotherSeatIsFree = false;
+        int seekedRow = 0;
         int max = -1;
-        int maxinRow = -1;
-        int wynik = 0;
 
-        for (int i = 0; i <= 0; i++) {
-            for (int j = 0; j < cinema[i].length-1; j++) {
-
-
-                booleanForTest = cinema[i][j] == 0 && cinema[i][j+1] == 0;
-
-
-                if (booleanForTest == true) {
-                    wynik++;
-                    if(wynik==1){wynik++;}
-                    if (wynik > max) {
-                        max = wynik;
-                    }
-                } else {
-                    wynik = 0;
-                }
-
-            }
-        }
-        System.out.println(max);
-
-        System.out.println("=======test======");
-        for (int i = 0; i < cinema.length - 1; i++) {
+        for (int i = 0; i < cinema.length; i++) {
             for (int j = 0; j < cinema[i].length - 1; j++) {
 
-                if (i != 0) {
-                    found0InARow = 0;
-                    booleanForTest = false;
-                    anotherZero = 0;
-                    currentRowTEST++;
-                }
-                booleanForTest = cinema[i][j] == 0 && cinema[i + 1][j + 1] == 0;
+                if (howManyTickersNeeded == 1) {
+                    if (cinema[i][j] == 0) {
 
-                if (booleanForTest == true) {
-                    anotherZero++;
-                }
+                        if (stopSearching != true) {
+                            seekedRow = (currentRow + 1);
+                        }
+                        stopSearching = true;
+                    }
+                } else {
 
+
+                    if (i != currentRow) {
+                        avaliableSeatsNextToEachOther = 0;
+                        currentRow++;
+
+                    } else {
+
+                        anotherSeatIsFree = cinema[i][j] == 0 && cinema[i][j + 1] == 0;
+
+
+                        if (anotherSeatIsFree == true) {
+
+                            avaliableSeatsNextToEachOther++;
+
+                            if (avaliableSeatsNextToEachOther == 1) {
+                                avaliableSeatsNextToEachOther++;
+                            }
+
+
+                            if (avaliableSeatsNextToEachOther > max) {
+                                max = avaliableSeatsNextToEachOther;
+                            }
+
+                            if (max == howManyTickersNeeded) {
+                                seekedRow = (currentRow + 1);
+                            }
+
+
+                        } else {
+                            avaliableSeatsNextToEachOther = 0;
+                        }
+
+                    }
+
+
+                }
 
             }
         }
 
-        System.out.println(max + "--- " + anotherZero);
-        System.out.println("========test ends=======");
-//
-//
-//        int rows = 16;
-//        int column = 18;
-//
-//
-//        int howManyTickers = 8;
-//        System.out.println("potrzebujemy wolnych kolo siebie: " + howManyTickers);
-//        int counter = 0;
-//        int moreThanOne = 0;
-//
-//
-//        int currentRow = 0;
-//        boolean stopSearching = false;
-//        int answer = 0;
-//
-//
-//        for (int i = 0; i < cinema.length; i++) {
-//            for (int j = 0; j < cinema[i].length - 1; j++) {
-//
-////                if (howManyTickers == 1) {
-////                    if (cinema[i][j] == 0) {
-////
-////                        if (stopSearching != true) {
-////                            answer = (currentRow + 1);
-////                        }
-////                        stopSearching = true;
-////                    }
-////                } else {
-//
-//
-//                if (i != currentRow) {
-//                    counter = 0;
-//                    moreThanOne = 0;
-//                    currentRow++;
-//
-//
-//                } else {
-//
-//                    if (cinema[i][j] == 0) {
-//                        counter++;
-//                        if (cinema[i][j + 1] == 0) {
-//                            moreThanOne++;
-//                            if ((moreThanOne + 1) == howManyTickers) {
-//
-//
-//                                if (stopSearching != true) {
-//                                    answer = (currentRow + 1);
-//                                }
-//                                stopSearching = true;
-//
-//
-//                            }
-//
-//                        }
-//                    }
-//                }
-//
-//
-//            }
-//
-//        }
-//        //}
-//
-//
-//        System.out.println(answer);
-//        //  System.out.println(answer != 0 ? answer : 0);
+
+        System.out.println(seekedRow);
 
     }
 }
